61ae9ba7c72e9538f0e6daa6d457f8d5a9897095
Samuel Asamoah <aleumas25@gmail.com>
2024-10-22 18:48:56 -0400

    add test

--- src/test/java/bank/utils/FeesCalculatorTest.java
+++ src/test/java/bank/utils/FeesCalculatorTest.java
@@ -5,6 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
 
 // Use of Parameterized helps in this case, since multiple runs of same test are required
 class FeesCalculatorTest {
@@ -24,4 +28,16 @@ class FeesCalculatorTest {
 		//assertEquals(0.01, calculator.calculateWithdrawalFee(200, 1000, false, 0));	//fail
 	}
 
+	@ParameterizedTest
+	@CsvSource({
+			"25, 250, true, 0.0",
+			"200, 1000, false, 20",
+			"300, 3000, true, 150.0",
+			"-10, 7000, false, -1",
+			"25, 20000, true, 12.5",
+			"200, -10, false, 20"
+	})
+	void depositTest(double amount, double accountBalance, boolean student, double expected) {
+		assertEquals(expected, calculator.calculateDepositInterest(amount, accountBalance, student));
+	}
 }
